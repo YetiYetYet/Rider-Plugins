324e8a6d14937fff81c618d9f62cec6582dd258f
GitHub Action <action@github.com>
2022-02-21 19:45:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/yetiyetyet/riderplugins/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yetiyetyet/riderplugins/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yetiyetyet.riderplugins.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yetiyetyet.riderplugins.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/yetiyetyet/riderplugins/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yetiyetyet/riderplugins/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yetiyetyet.riderplugins.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yetiyetyet.riderplugins.MyBundle
 
 class MyProjectService(project: Project) {
 
